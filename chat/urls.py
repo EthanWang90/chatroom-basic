9d27574fb88a497cf500816fc3c07b8f078e1c36
ethan <ethanwang9011@gmail.com>
2019-04-04 10:51:02 +1300

    add chat room template

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, room
+from django.conf.urls import url
 
 app_name="chat"
 urlpatterns = [
     path('', index, name='index'),
+    url(r'^(?P<room_name>[^/]+)/$', room, name='room'),
 ]
